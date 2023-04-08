74c690c7cf4e8f410bee3286643aaf2ea4d0ad51
Prae Songprasit <prae.songprasit@xero.com>
2023-04-08 17:54:57 +1200

    Added newsletter signup

---
+++ netlify/functions/submission-created.js
@@ -0,0 +1,29 @@
+// Based on guide by Andew Stiefel
+// https://andrewstiefel.com/netlify-functions-email-subscription/
+
+import fetch from 'node-fetch';
+const { SITE_EDM_API_KEY, NZTR_NEWSLETTER_LIST_ID } = process.env;
+
+exports.handler = async (event) => {
+  const email = JSON.parse(event.body).payload.email;
+
+  const response = await fetch(
+    `https://emailoctopus.com/api/1.6/lists/${NZTR_NEWSLETTER_LIST_ID}/contacts`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ 
+        api_key: SITE_EDM_API_KEY,
+        email_address: email
+      }),
+    }
+  );
+
+  let responseText = await response.text();
+
+  return {
+    statusCode: 200,
+    body: responseText,
+  }
+};
