68e4715c3b4613b5cce25df952f2311a3177e9e8
Prae Songprasit <prae.songprasit@xero.com>
2023-04-27 19:07:26 +1200

    Corrected date/time formatting to fix 1 day off

--- .eleventy.js
+++ .eleventy.js
@@ -79,7 +79,7 @@ module.exports = function (eleventyConfig) {
   https://github.com/moment/luxon/blob/master/docs/formatting.md#the-basics
   // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/time
   eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLLL yyyy");
+    return DateTime.fromJSDate(dateObj).toFormat("dd LLLL yyyy");
   });
 
   eleventyConfig.addFilter("readableYear", dateObj => {
@@ -91,7 +91,7 @@ module.exports = function (eleventyConfig) {
   });
 
   eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+    return DateTime.fromJSDate(dateObj).toFormat('yyyy-LL-dd');
   });
 
   eleventyConfig.addCollection("speakers", collection => {
